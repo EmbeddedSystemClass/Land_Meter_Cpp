ba866e83b4d8901133a94aa58ab80521735a11cd
Jack <jackw2050@gmail.com>
2018-09-13 11:33:46 -0500

    Changed files for initial c++ learning

---
+++ Land2018/src/Land2018.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Land2018.cpp
+// Author      : Jack Walker
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
---
+++ src/ZLS_LAND.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : ZLS_LAND.cpp
+// Author      : Jack Walker
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "gpio/GPIO.h"
+#include "gpio/PWM.h"
+#include "PWM.h"
+#include "util.h"
+
+
+using namespace std;
+using namespace exploringBB;
+
+
+int main() {
+//	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
