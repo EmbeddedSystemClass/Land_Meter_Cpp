5ab00be8b51b7e3ea2f90e979cb4616c188ce52c
Jack <jackw2050@gmail.com>
2018-09-26 11:07:17 -0500

    updated PDFs

--- Land2018/Debug/src/subdir.mk
+++ Land2018/Debug/src/subdir.mk
@@ -4,13 +4,34 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/Land2018.cpp 
+../src/GPIO.cpp \
+../src/Land2018.cpp \
+../src/encoder.cpp \
+../src/heater.cpp \
+../src/max11100_ADC.cpp \
+../src/pthreads.cpp \
+../src/pwm.cpp \
+../src/util.cpp 
 
 OBJS += \
-./src/Land2018.o 
+./src/GPIO.o \
+./src/Land2018.o \
+./src/encoder.o \
+./src/heater.o \
+./src/max11100_ADC.o \
+./src/pthreads.o \
+./src/pwm.o \
+./src/util.o 
 
 CPP_DEPS += \
-./src/Land2018.d 
+./src/GPIO.d \
+./src/Land2018.d \
+./src/encoder.d \
+./src/heater.d \
+./src/max11100_ADC.d \
+./src/pthreads.d \
+./src/pwm.d \
+./src/util.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
